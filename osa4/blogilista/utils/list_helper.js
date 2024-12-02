5c6babd22f82114e97c18c8d66af07712dbe6bce
Tommi Kiiski <kiiski.tommi@hotmail.com>
2024-12-02 09:39:00 +0200

    complete 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,14 +1,33 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
 const totalLikes = (blogs) => {
-  res = blogs.length === 0 ? 0 : blogs.reduce((p,t) => p+t.likes, 0)
+  const res = blogs.length === 0 ? 0 : blogs.reduce((p,t) => p+t.likes, 0)
   return res
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0){
+    return null
+  }
+  var max = 0
+  var ret = {}
+  blogs.forEach(blog => {
+    if (blog.likes > max) {
+      ret = blog
+      max = blog.likes
+    }
+  })
+  return {
+    title: ret.title,
+    author: ret.author,
+    likes: ret.likes
+  }
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
